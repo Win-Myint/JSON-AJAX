77b5acd34e9fc28ed8cf5ff2d0982bee43ee0914
Win <win.m179@gmail.com>
2017-03-11 19:07:20 +0000

    successfully send a request to the url and retrieved JSON data

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 log = console.log;
-log("Hellow world!");
+
+// make an instance of browser built in XMLHttpRequest object
+var myRequest = new XMLHttpRequest();
+
+// initialise a request using open() method
+myRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+
+// use onload() method to check the request 
+// change successfully retrieved JSON data to an object and log it to the console
+myRequest.onload = function() {
+	var data = JSON.parse(myRequest.responseText);
+	log(data[0]);
+}
+
+// send the request 
+myRequest.send();
